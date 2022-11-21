682c76370271342862f2dd3a23d77c312db5f032
jose700 <josecampu28@gmail.com>
2022-11-20 19:53:18 -0500

    ejercicio corva

---
+++ project_corba/HelloApp/Hello.java
@@ -0,0 +1,7 @@
+package HelloApp;
+
+
+
+public interface Hello extends HelloOperations, org.omg.CORBA.Object, org.omg.CORBA.portable.IDLEntity 
+{
+} // interface Hello
---
+++ project_corba/HelloApp/HelloOperations.java
@@ -0,0 +1,10 @@
+package HelloApp;
+
+
+
+
+public interface HelloOperations 
+{
+  String sayHello ();
+  void shutdown ();
+} // interface HelloOperations
